c9611bcc7a50d43d1717f73f84f761f32619f0f0
brahimbounguicha <44786262+brahimbounguicha@users.noreply.github.com>
2024-05-27 22:27:41 +0200

    je change config.java

--- src/test/java/Helper/Config.java
+++ src/test/java/Helper/Config.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.interactions.Actions;
 public class Config {
 	public static WebDriver driver;
 	public static Actions action;
+public static WebDriver driver2;
+public static WebDriver driver3;
 	
 	public static void confchrome() throws Exception {
 		System.setProperty("webdriver.chrome.driver",Utils.getproprety("TestDriverPath"));
